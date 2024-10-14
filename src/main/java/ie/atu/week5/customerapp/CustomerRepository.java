192522c5369a224c6e8398478398bb6ab8f566cf
Gavin <G00402296@atu.ie>
2024-10-14 13:25:09 +0100

    Code me thinks!

--- src/main/java/ie/atu/week5/customerapp/CustomerRepository.java
+++ src/main/java/ie/atu/week5/customerapp/CustomerRepository.java
@@ -2,5 +2,8 @@ package ie.atu.week5.customerapp;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends MongoRepository<Customer, String> {
+    public Optional<Customer> findByEmail(String email);
 }
--- src/main/java/ie/atu/week5/customerapp/Order.java
+++ src/main/java/ie/atu/week5/customerapp/Order.java
@@ -1,6 +1,7 @@
 package ie.atu.week5.customerapp;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -11,8 +12,11 @@ public class Order {
 
     @Id
     private String id;
+    @PositiveOrZero
     private int orderCode;
+    @NotBlank
     private String orderDetails;
     private String orderDate;
+    @NotBlank
     private String customerId; // Reference to the associated customer
 }
